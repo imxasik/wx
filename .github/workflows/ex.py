e4084c598610a1b422d79b58848ed99b8c95820b
imxasik <99292014+imxasik@users.noreply.github.com>
2023-10-01 12:44:02 +0600

    Create ex.py

---
+++ .github/workflows/ex.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import os
+
+fig, ax = plt.subplots()
+
+fruits = ['apple', 'blueberry', 'cherry', 'orange']
+counts = [40, 100, 30, 55]
+bar_labels = ['red', 'blue', '_red', 'orange']
+bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+
+ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
+
+ax.set_ylabel('fruit supply')
+ax.set_title('Fruit supply by kind and color')
+ax.legend(title='Fruit color')
+
+# Use a relative path to save the output.png file in the same directory as the script
+output_path = os.path.join(os.path.dirname(__file__), 'output.png')
+plt.savefig(output_path)
