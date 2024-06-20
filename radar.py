abb3ce2a04c1a15994f1526a82c417576b3c9367
imxasik <99292014+imxasik@users.noreply.github.com>
2024-06-20 13:47:18 +0600

    Update radar.py

--- radar.py
+++ radar.py
@@ -1,51 +1,30 @@
 import requests
 import io
-import matplotlib.pyplot as plt
-import ftplib
-from bs4 import BeautifulSoup
+from ftplib import FTP
 
-# URL of the website
-url = "https://data.rainviewer.com/images/BDCOMP/"
+# Step 1: Define the URL of the image
+image_url = 'https://wx.baf.mil.bd/METBSR/images/omar/RadarSingle/mtr.jpg'
 
-# Send an HTTP GET request to the URL
-response = requests.get(url)
+# Step 2: Download the image
+response = requests.get(image_url)
 
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, 'html.parser')
-
-# Find all anchor tags with href attribute ending with "source.jpeg"
-anchor_tags = soup.find_all('a', href=lambda href: href and href.endswith("source.jpeg"))
-
-if anchor_tags:
-    latest_source_link = anchor_tags[-1]['href']
-    latest_source_url = url + latest_source_link
-
-    # Download the latest source.jpeg
-    file_content = requests.get(latest_source_url).content
-
-    # Save the downloaded file
-    with open("mos.jpg", "wb") as f:
-        f.write(file_content)
-    print("Latest source.jpeg downloaded successfully.")
-
-    # Plot your data
-    # Assuming you have already created a plot here
-
-    # Save the plot to a buffer
-    plot_buffer = io.BytesIO()
-    plt.savefig(plot_buffer, format="jpg", transparent=True)
-    plot_buffer.seek(0)
+# Check if the request was successful
+if response.status_code == 200:
+    # Save the image temporarily in memory
+    image_data = io.BytesIO(response.content)
 
     # FTP Server Details
     ftp_host = "ftpupload.net"
     ftp_username = "epiz_32144154"
     ftp_password = "Im80K123"
+    ftp_target_path = "htdocs/wx/mtr.jpg"  # Adjust the path as needed
 
-    # Connect to the FTP server and upload the plot directly
-    with ftplib.FTP(ftp_host) as ftp:
+    # Step 3: Upload the image to your hosting server using FTP
+    with FTP(ftp_host) as ftp:
         ftp.login(ftp_username, ftp_password)
-        ftp.cwd('htdocs/radar')  # Change directory to your desired location on the FTP server
-        ftp.storbinary('STOR com.jpg', plot_buffer)
-        print("Plot uploaded successfully to FTP server.")
+        ftp.cwd('htdocs/wx')  # Change directory to your desired location on the FTP server
+        ftp.storbinary('STOR mtr.jpg', image_data)
+
+    print("Image uploaded successfully.")
 else:
-    print("No source.jpeg files found on the website.")
+    print("Failed to fetch the image from the website. Status code:", response.status_code)
