10d5154b623b5d015b1a99c991d3a01f6fdc64f7
imxasik <99292014+imxasik@users.noreply.github.com>
2024-09-27 15:42:48 +0600

    Update tc_al.py

--- tc_al.py
+++ tc_al.py
@@ -80,8 +80,8 @@ def plot_cyclone_track(track_data, cyclone_id):
     y_range = last_lat + 4 - (first_lat - 3)
 
     # Set the figure size based on the aspect ratio of the plot
-    aspect_ratio = x_range / y_range
-    fig_width = 18  # Fixed width
+    aspect_ratio = y_range / x_range
+    fig_width = 15  # Fixed width
     fig_height = fig_width / aspect_ratio
 
     # Adjust figure size dynamically based on calculated height
