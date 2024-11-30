94d45d211991b18459ae1800b4b89d87c085e1d7
imxasik <99292014+imxasik@users.noreply.github.com>
2024-11-30 12:22:17 +0600

    Update old_tc.py

--- old_tc.py
+++ old_tc.py
@@ -13,7 +13,7 @@ import io
 
 # Editable options
 basin = "al"    # Basin code (
-year = "2021"   # Seasonal Year
+year = "2022"   # Seasonal Year
 start_id = 1    # Starting TC ID
 end_id = 20      # Ending TC ID
 
